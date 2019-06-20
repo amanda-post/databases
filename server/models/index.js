5c64fa7972201e3d01e5692c796175d52f30b685
amanda-post <amanda.post44@gmail.com>
2019-06-20 12:26:43 -0700

    now deals with duplicates

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ module.exports = {
   messages: {
     // a function which produces all the messages
     get: function (callback) {
-      var qry = 'SELECT * FROM messages'; // do we want this to be * instead of message?
+      var qry = 'SELECT * FROM messages'; 
       db.query(qry, (err, results) => {
         if (err) {
           console.log('err from query to get messages', err);
@@ -15,19 +15,7 @@ module.exports = {
       });
     }, 
 
-    // a function which can be used to insert a message into the database
-  //   post: function (input) {
-  //     var qry = `INSERT INTO messages(message, user_id, room_id) VALUES("${input.inputMessage}", "${input.inputUser}", "${input.inputRoom}")`;
-  //     db.query(qry, (err) => {
-  //       if (err) {
-  //         console.log('err from query to post message', err);
-  //       }
-  //     });
-  //   } 
-  // },
-
-
-  post: function (input) {
+    post: function (input) {
       var qryRooms = `INSERT INTO rooms (name) VALUES("${input.inputRoom}") ON DUPLICATE KEY UPDATE id=id`;
       var qryUsers = `INSERT INTO users (name) VALUES("${input.inputUser}") ON DUPLICATE KEY UPDATE id=id`;
       var qryMessages = `INSERT INTO messages(message, user_id, room_id) VALUES("${input.inputMessage}", (SELECT id FROM rooms WHERE name = "${input.inputRoom}"), (SELECT id FROM users WHERE name = "${input.inputUser}") )`;
@@ -50,22 +38,6 @@ module.exports = {
       });
     } 
   },
-
-  /*
-1) check rooms
-
-2) check users
-INSERT INTO
-
-inputMessage = "sigj;dfgjl;skdf"
-inputRoom = 'lobby'
-inputUser = 'amanda'
-
-3) insert the message with foreign keys
-INSERT INTO messages(message, user_id, room_id) 
-VALUES("${input.inputMessage}", (SELECT id FROM rooms WHERE name = ${input.inputRoom}), (SELECT id FROM users WHERE name = ${input.inputUser}) )`;
-
-  */
 
   users: {
     // Ditto as above.
